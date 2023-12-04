d7cf2be43b33d915fed55efa61fc5ad060a5bb6a
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-12-04 10:52:15 +0900

    FEAT
    add get My Product Info API
    
    SCRUM-48

--- src/main/java/com/jeontongju/product/controller/ProductRestController.java
+++ src/main/java/com/jeontongju/product/controller/ProductRestController.java
@@ -2,7 +2,9 @@ package com.jeontongju.product.controller;
 
 import com.jeontongju.product.dto.request.ProductDto;
 import com.jeontongju.product.dto.response.CategoryDto;
+import com.jeontongju.product.dto.response.MyProductInfoDto;
 import com.jeontongju.product.dto.temp.ResponseFormat;
+import com.jeontongju.product.enums.temp.MemberRoleEnum;
 import com.jeontongju.product.service.ProductService;
 import java.util.List;
 import javax.validation.Valid;
@@ -33,7 +35,9 @@ public class ProductRestController {
 
   @PostMapping("/products")
   public ResponseEntity<ResponseFormat<Void>> createProduct(
-          @Valid @RequestBody ProductDto product, @RequestHeader Long memberId, String memberRole) {
+      @Valid @RequestBody ProductDto product,
+      @RequestHeader Long memberId,
+      @RequestHeader MemberRoleEnum memberRole) {
 
     productService.createProduct(memberId, product);
 
@@ -43,6 +47,20 @@ public class ProductRestController {
                 .code(HttpStatus.OK.value())
                 .message(HttpStatus.OK.name())
                 .detail("상품 등록 성공")
+                .build());
+  }
+
+  @GetMapping("/sellers/info/product")
+  public ResponseEntity<ResponseFormat<List<MyProductInfoDto>>> getMyProductInfo(
+      @RequestHeader Long memberId, @RequestHeader MemberRoleEnum memberRole) {
+
+    return ResponseEntity.ok()
+        .body(
+            ResponseFormat.<List<MyProductInfoDto>>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.name())
+                .detail("내 상품 정보 조회 성공")
+                .data(productService.getMyProductInfo(memberId))
                 .build());
   }
 }
