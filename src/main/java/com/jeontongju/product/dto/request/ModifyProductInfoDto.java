da76b2a426a083618f453e8227638cbbdc944e12
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-12-12 10:24:31 +0900

    FIX
    add validation at productDto, ModifyProductInfoDto

--- src/main/java/com/jeontongju/product/dto/request/ModifyProductInfoDto.java
+++ src/main/java/com/jeontongju/product/dto/request/ModifyProductInfoDto.java
@@ -1,5 +1,6 @@
 package com.jeontongju.product.dto.request;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import lombok.*;
 
@@ -15,8 +16,10 @@ public class ModifyProductInfoDto {
 
   private String productThumbnailImageUrl;
 
+  @Min(value = 1, message = "1원 이상만 입력 가능합니다.")
   private Long productPrice;
 
+  @Min(value = 1, message = "1개 이상만 입력 가능합니다.")
   private Long registeredQuantity;
 
   private String productDetailsImageUrl;
--- src/main/java/com/jeontongju/product/dto/request/ProductDto.java
+++ src/main/java/com/jeontongju/product/dto/request/ProductDto.java
@@ -1,11 +1,9 @@
 package com.jeontongju.product.dto.request;
 
-import java.util.List;
-
 import com.jeontongju.product.vo.Taste;
-import lombok.*;
-
+import java.util.List;
 import javax.validation.constraints.*;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,9 +23,11 @@ public class ProductDto {
   private String productThumbnailImageUrl;
 
   @NotNull(message = "null 이 불가합니다.")
+  @DecimalMin(value = "0.0", message = "0 이상만 입력 가능합니다.")
   private Double productAlcoholDegree;
 
   @NotNull(message = "null 이 불가합니다.")
+  @Min(value = 1, message = "1 이상만 입력 가능합니다.")
   private Long productCapacity;
 
   @NotBlank(message = "내용은 null 이 불가합니다.")
@@ -45,9 +45,11 @@ public class ProductDto {
   private String manufacturer;
 
   @NotNull(message = "null 이 불가합니다.")
+  @Min(value = 1, message = "1원 이상만 입력 가능합니다.")
   private Long productPrice;
 
   @NotNull(message = "null 이 불가합니다.")
+  @Min(value = 1, message = "1개 이상만 입력 가능합니다.")
   private Long registeredQuantity;
 
   private String productDetailsImageUrl;
@@ -58,10 +60,10 @@ public class ProductDto {
   @NotNull(message = "null 이 불가합니다.")
   private Taste taste;
 
+  @Size(max = 2, message = "최대 2개의 항목을 가져야 합니다.")
   private List<String> rawMaterial;
 
   private List<String> food;
 
   private List<String> concept;
-
 }
