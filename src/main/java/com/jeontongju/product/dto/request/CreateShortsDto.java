cdd89cc28b995899046115c813232c608ac60b37
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-12-29 12:09:46 +0900

    FIX

--- src/main/java/com/jeontongju/product/dto/request/CreateShortsDto.java
+++ src/main/java/com/jeontongju/product/dto/request/CreateShortsDto.java
@@ -12,24 +12,24 @@ import lombok.*;
 public class CreateShortsDto {
 
   @Size(min = 1, max = 30, message = "내용은 1글자 이상 30 글자 이하만 입력 가능합니다.")
-  @NotBlank(message = "null 이 불가합니다.")
+  @NotNull(message = "null 이 불가합니다.")
   private String shortsTitle;
 
   @Size(min = 1, max = 300, message = "내용은 1글자 이상 300 글자 이하만 입력 가능합니다.")
-  @NotBlank(message = "null 이 불가합니다.")
+  @NotNull(message = "null 이 불가합니다.")
   private String shortsDescription;
 
-  @NotBlank(message = "내용은 null 이 불가합니다.")
+  @NotNull(message = "null 이 불가합니다.")
   private String shortsPreviewUrl;
 
-  @NotBlank(message = "내용은 null 이 불가합니다.")
+  @NotNull(message = "null 이 불가합니다.")
   private String shortsVideoUrl;
 
-  @NotBlank(message = "내용은 null 이 불가합니다.")
+  @NotNull(message = "null 이 불가합니다.")
   private String shortsThumbnailUrl;
 
   private String productId;
 
-  @NotNull(message = "내용은 null 이 불가합니다.")
+  @NotNull(message = "null 이 불가합니다.")
   private Boolean isActivate;
 }
